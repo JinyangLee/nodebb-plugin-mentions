05f79f694f23606a2325501a04cf4f9e5a943ff9
lijinyang <lithium@lijinyangs-iMac.local>
2016-10-08 16:59:47 +0800

    [update]

--- library.js
+++ library.js
@@ -1,29 +1,24 @@
 'use strict';
 
-var	async = module.parent.require('async');
-var S = module.parent.require('string');
-var winston = module.parent.require('winston');
-var XRegExp = module.parent.require('xregexp');
-var validator = module.parent.require('validator');
-var nconf = module.parent.require('nconf');
+var	async = require('async'),
+	XRegExp = require('xregexp').XRegExp,
 
-var Topics = module.parent.require('./topics');
-var User = module.parent.require('./user');
-var Groups = module.parent.require('./groups');
-var Notifications = module.parent.require('./notifications');
-var Privileges = module.parent.require('./privileges');
-var Utils = module.parent.require('../public/src/utils');
+	nconf = module.parent.require('nconf'),
+	Topics = module.parent.require('./topics'),
+	User = module.parent.require('./user'),
+	Groups = module.parent.require('./groups'),
+	Notifications = module.parent.require('./notifications'),
+	Utils = module.parent.require('../public/src/utils'),
 
-var SocketPlugins = module.parent.require('./socket.io/plugins');
+	SocketPlugins = module.parent.require('./socket.io/plugins'),
 
-var regex = XRegExp('(?:>|\\s)(@[\\p{L}\\d\\-_.]+)', 'g');	// used in post text transform, accounts for HTML
-var rawRegex = XRegExp('(?:^|\\s)(@[\\p{L}\\d\-_.]+)', 'g');	// used in notifications, as raw text is passed in this hook
-var isLatinMention = /@[\w\d\-_.]+$/;
-var removePunctuationSuffix = function(string) {
-	return string.replace(/[!?.]*$/, '');
-};
+	regex = XRegExp('(@[\\p{L}\\d\\-_.]+)', 'g'),
+	isLatinMention = /@[\w\d\-_.]+$/,
+	removePunctuationSuffix = function(string) {
+		return string.replace(/[!?.]*$/, '');
+	},
 
-var Mentions = {};
+	Mentions = {};
 
 SocketPlugins.mentions = {};
 
@@ -39,7 +34,8 @@ Mentions.notify = function(postData) {
 	}
 
 	var cleanedContent = Mentions.clean(postData.content, true, true, true);
-	var matches = cleanedContent.match(rawRegex);
+	var matches = cleanedContent.match(regex);
+	console.log('new');
 
 	if (!matches) {
 		return;
@@ -48,34 +44,31 @@ Mentions.notify = function(postData) {
 	var noMentionGroups = ['registered-users', 'guests'];
 
 	matches = matches.map(function(match) {
-		return Utils.slugify(match);
+		var slugReg = new RegExp('<a href="/community/user/([^"]*)">(' + match + ')</a>');
+		console.log(cleanedContent.match(slugReg)[1]);
+		return cleanedContent.match(slugReg)[1];
+		//return Utils.slugify(match.slice(1));
 	}).filter(function(match, index, array) {
 		return match && array.indexOf(match) === index && noMentionGroups.indexOf(match) === -1;
 	});
-
-	if (!matches.length) {
-		return;
-	}
+	console.log(matches);
 
 	async.parallel({
 		userRecipients: function(next) {
-			filter(matches, User.existsBySlug, next);
+			filter(matches, User.exists, next);
 		},
 		groupRecipients: function(next) {
-			filter(matches, Groups.existsBySlug, next);
+			filter(matches, Groups.exists, next);
 		}
 	}, function(err, results) {
 		if (err) {
 			return;
 		}
-
-		if (!results.userRecipients.length && !results.groupRecipients.length) {
-			return;
-		}
+		console.log(results);
 
 		async.parallel({
 			topic: function(next) {
-				Topics.getTopicFields(postData.tid, ['title', 'cid'], next);
+				Topics.getTopicFields(postData.tid, ['title'], next);
 			},
 			author: function(next) {
 				User.getUserField(postData.uid, 'username', next);
@@ -85,144 +78,86 @@ Mentions.notify = function(postData) {
 					User.getUidByUserslug(slug, next);
 				}, next);
 			},
-			groupData: function(next) {
+			groupsMembers: function(next) {
 				getGroupMemberUids(results.groupRecipients, next);
-			},
-			topicFollowers: function(next) {
-				Topics.getFollowers(postData.tid, next);
 			}
 		}, function(err, results) {
 			if (err) {
 				return;
 			}
 
-			var title = S(results.topic.title).decodeHTMLEntities().s;
-			var titleEscaped = title.replace(/%/g, '&#37;').replace(/,/g, '&#44;');
-
-			var uids = results.uids.filter(function(uid, index, array) {
-				return array.indexOf(uid) === index && parseInt(uid, 10) !== parseInt(postData.uid, 10) && results.topicFollowers.indexOf(uid.toString()) === -1;
+			var uids = results.uids.concat(results.groupsMembers).filter(function(uid, index, array) {
+				return array.indexOf(uid) === index && parseInt(uid, 10) !== parseInt(postData.uid, 10);
 			});
 
-			var groupMemberUids = [];
-			results.groupData.groupNames.forEach(function(groupName, index) {
-				results.groupData.groupMembers[index] = results.groupData.groupMembers[index].filter(function(uid, index, array) {
-					return array.indexOf(uid) === index &&
-						uids.indexOf(uid) === -1 &&
-						parseInt(uid, 10) !== parseInt(postData.uid, 10) &&
-						results.topicFollowers.indexOf(uid.toString()) === -1 &&
-						groupMemberUids.indexOf(uid) === -1;
+			if (uids.length > 0) {
+				Notifications.create({
+					bodyShort: '[[notifications:user_mentioned_you_in, ' + results.author + ', ' + results.topic.title + ']]',
+					bodyLong: postData.content,
+					nid: 'tid:' + postData.tid + ':pid:' + postData.pid + ':uid:' + postData.uid,
+					pid: postData.pid,
+					tid: postData.tid,
+					from: postData.uid,
+					importance: 6
+				}, function(err, notification) {
+					if (err || !notification) {
+						return;
+					}
+					Notifications.push(notification, results.uids);
 				});
-				groupMemberUids = groupMemberUids.concat(results.groupData.groupMembers[index]);
-			});
-
-			sendNotificationToUids(postData, uids, 'user', '[[notifications:user_mentioned_you_in, ' + results.author + ', ' + titleEscaped + ']]');
-			results.groupData.groupNames.forEach(function(groupName, index) {
-				var memberUids = results.groupData.groupMembers[index];
-				sendNotificationToUids(postData, memberUids, groupName, '[[notifications:user_mentioned_group_in, ' + results.author + ', ' + groupName + ', ' + titleEscaped + ']]');
-			});
+			}
 		});
 	});
 };
-
-function sendNotificationToUids(postData, uids, nidType, notificationText) {
-	if (!uids.length) {
-		return;
-	}
-
-	async.waterfall([
-		function(next) {
-			Privileges.topics.filterUids('read', postData.tid, uids, next);
-		},
-		function(_uids, next) {
-			uids = _uids;
-			if (!uids.length) {
-				return;
-			}
-			createNotification(postData, nidType, notificationText, next);
-		},
-		function(notification, next) {
-			if (notification) {
-				Notifications.push(notification, uids);
-			}
-			next();
-		}
-	], function(err) {
-		if (err) {
-			return winston.error(err);
-		}
-	});
-}
-
-function createNotification(postData, nidType, notificationText, callback) {
-	Notifications.create({
-		bodyShort: notificationText,
-		bodyLong: postData.content,
-		nid: 'tid:' + postData.tid + ':pid:' + postData.pid + ':uid:' + postData.uid + ':' + nidType,
-		pid: postData.pid,
-		tid: postData.tid,
-		from: postData.uid,
-		path: '/post/' + postData.pid,
-		importance: 6
-	}, callback);
-}
 
 function getGroupMemberUids(groupRecipients, callback) {
 	async.map(groupRecipients, function(slug, next) {
 		Groups.getGroupNameByGroupSlug(slug, next);
-	}, function(err, groupNames) {
+	}, function(err, groups) {
 		if (err) {
 			return callback(err);
 		}
-		async.map(groupNames, function(groupName, next) {
-			Groups.getMembers(groupName, 0, -1, next);
-		}, function(err, groupMembers) {
+		async.map(groups, function(group, next) {
+			Groups.getMembers(group, 0, -1, next);
+		}, function(err, results) {
 			if (err) {
 				return callback(err);
 			}
-			callback(null, {groupNames: groupNames, groupMembers: groupMembers});
+
+			var uids = [];
+			results.forEach(function(members) {
+				uids = uids.concat(members);
+			});
+			uids = uids.filter(function(uid, index, array) {
+				return parseInt(uid, 10) && array.indexOf(uid) === index;
+			});
+			callback(null, uids);
 		});
 	});
 }
 
-Mentions.parsePost = function(data, callback) {
-	if (!data || !data.postData || !data.postData.content) {
+Mentions.addMentions = function(data, callback) {
+	var relativeUrl = nconf.get('relative_path') || '',
+		originalContent, cleanedContent;
+
+	if (data && typeof data === 'string') {
+		originalContent = data;
+		cleanedContent = Mentions.clean(data, false, false, true);
+	} else if (!data || !data.postData || !data.postData.content) {
+		return callback(null, data);
+	} else {
+		originalContent = data.postData.content;
+		cleanedContent = Mentions.clean(data.postData.content, false, false, true);
+	}
+
+	var matches = cleanedContent.match(regex);
+
+	if (!matches) {
 		return callback(null, data);
 	}
-
-	Mentions.parseRaw(data.postData.content, function(err, content) {
-		if (err) {
-			return callback(err);
-		}
-
-		data.postData.content = content;
-		callback(null, data);
-	});
-};
-
-Mentions.parseRaw = function(content, callback) {
-	var splitContent = Mentions.split(content, false, false, true);
-	var matches = [];
-	splitContent.forEach(function(cleanedContent, i) {
-		if ((i & 1) === 0) {
-			matches = matches.concat(cleanedContent.match(regex) || []);
-		}
-	});
-
-	if (!matches.length) {
-		return callback(null, content);
-	}
-
+	// Eliminate duplicates
 	matches = matches.filter(function(cur, idx) {
-		// Eliminate duplicates
 		return idx === matches.indexOf(cur);
-	}).map(function(match) {
-		/**
-		 *	Javascript-favour of regex does not support lookaround,
-		 *	so need to clean up the cruft by discarding everthing
-		 *	before the @
-		 */
-		var atIndex = match.indexOf('@');
-		return atIndex !== 0 ? match.slice(atIndex) : match;
 	});
 
 	async.each(matches, function(match, next) {
@@ -231,61 +166,48 @@ Mentions.parseRaw = function(content, callback) {
 		match = removePunctuationSuffix(match);
 
 		async.parallel({
-			groupExists: async.apply(Groups.existsBySlug, slug),
+			groupName: async.apply(Groups.exists, slug),
 			uid: async.apply(User.getUidByUserslug, slug)
 		}, function(err, results) {
 			if (err) {
 				return next(err);
 			}
 
-			if (results.uid || results.groupExists) {
-				var regex = isLatinMention.test(match)
-					? new RegExp('[>|\\s]' + match + '\\b', 'g')
-					: new RegExp('[>|\\s]' + match, 'g');
+			if (results.uid) {
+				if (isLatinMention.test(match)) {
+					originalContent = originalContent.replace(new RegExp(match + '\\b', 'g'), '<a class="plugin-mentions-a" href="' + relativeUrl + '/user/' + slug + '">' + match + '</a>');
+				} else {
+					originalContent = originalContent.replace(new RegExp(match, 'g'), '<a class="plugin-mentions-a" href="' + relativeUrl + '/user/' + slug + '">' + match + '</a>');
+				}
+			} else if (results.groupName) {
+				originalContent = originalContent.replace(new RegExp(match + '\\b', 'g'), '<a class="plugin-mentions-a" href="' + relativeUrl + '/groups/' + slug + '">' + match + '</a>');
+			}
 
-				splitContent = splitContent.map(function(c, i) {
-					if ((i & 1) === 1) {
-						return c;
-					}
-					return c.replace(regex, function(match) {
-						// Again, cleaning up lookaround leftover bits
-						var atIndex = match.indexOf('@');
-						var plain = match.slice(0, atIndex);
-						match = match.slice(atIndex);
-						var str = results.uid
-								? '<a class="plugin-mentions-a" href="' + nconf.get('url') + '/uid/' + results.uid + '">' + match + '</a>'
-								: '<a class="plugin-mentions-a" href="' + nconf.get('url') + '/groups/' + slug + '">' + match + '</a>';
-
-						return plain + str;
-					});
-				});
+			if (data && typeof data === 'string') {
+				data = originalContent;
+			} else {
+				data.postData.content = originalContent;
 			}
 
 			next();
 		});
 	}, function(err) {
-		callback(err, splitContent.join(''));
+		callback(err, data);
 	});
 };
 
 Mentions.clean = function(input, isMarkdown, stripBlockquote, stripCode) {
-	var split = Mentions.split(input, isMarkdown, stripBlockquote, stripCode);
-	split = split.filter(function(e, i) {
-		// only keep non-code/non-blockquote
-		return (i & 1) === 0;
-	});
-	return split.join('');
-};
+	var bqMatch = isMarkdown ? /^>.*$/gm : /^<blockquote>.*<\/blockquote>/gm,
+		pfMatch = isMarkdown ? /`[^`\n]+`/gm : /<code>.*<\/code>/gm;
 
-Mentions.split = function(input, isMarkdown, splitBlockquote, splitCode) {
-	var matchers = [isMarkdown ? '\\[.*?\\]\\(.*?\\)' : '<a[\\s\\S]*?</a>'];
-	if (splitBlockquote) {
-		matchers.push(isMarkdown ? '^>.*$' : '^<blockquote>.*?</blockquote>');
+	if (stripBlockquote) {
+		input = input.replace(bqMatch, '');
 	}
-	if (splitCode) {
-		matchers.push(isMarkdown ? '`[^`\n]+`' : '<code[\\s\\S]*?</code>');
+	if (stripCode) {
+		input = input.replace(pfMatch, '');
 	}
-	return input.split(new RegExp('(' + matchers.join('|') + ')', 'gm'));
+
+	return input;
 };
 
 /*
@@ -293,14 +215,16 @@ Mentions.split = function(input, isMarkdown, splitBlockquote, splitCode) {
 */
 
 SocketPlugins.mentions.listGroups = function(socket, data, callback) {
-	Groups.getGroups('groups:visible:createtime', 0, -1, function(err, groups) {
-		if (err) {
-			return callback(err);
+	Groups.list({
+		removeEphemeralGroups: true,
+		truncateUserList: true
+	}, function(err, groups) {
+		if (err || !Array.isArray(groups)) {
+			return callback(null, []);
 		}
-		groups = groups.filter(Boolean).map(function(groupName) {
-			return validator.escape(groupName);
-		});
-		callback(null, groups);
+		callback(null, groups.map(function(groupObj) {
+			return groupObj.name;
+		}));
 	});
 };
 
--- static/autofill.js
+++ static/autofill.js
@@ -1,105 +1,100 @@
-"use strict";
-/* globals socket, app, utils */
-
-
 $(document).ready(function() {
-	var groupList = [];
-	var localUserList = [];
-
-	$(window).on('composer:autocomplete:init', function(ev, data) {
-		localUserList = loadDomUsers();
-
-		if (!groupList.length) {
-			loadGroupList();
-		}
-
-		var subset;
-		var strategy = {
-			match: /\B@([^\s\n]*)?$/,
-			search: function (term, callback) {
-				var usernames;
-				if (!term) {
-					usernames = localUserList.concat(groupList).filter(function(value, index, array) {
-						return array.indexOf(value) === index && value !== app.user.username;
-					}).sort(function(a, b) {
-						return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-					});
-					return callback(usernames);
-				}
-
-				socket.emit('user.search', {query: term}, function(err, userdata) {
-					if (err) {
-						return callback([]);
-					}
-
-					usernames = userdata.users.map(function(user) {
-						return user.username;
-					});
-
-					subset = localUserList.concat(groupList).filter(function(username) {
-						return username.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) !== -1;
-					});
-
-					usernames = usernames.concat(subset).filter(function(value, index, array) {
-						return array.indexOf(value) === index;
-					});
-
-					subset = subset.map(function(name) {
-						return name.toLocaleLowerCase();
-					});
-
-					usernames.sort(function(a, b) {
-						if (subset.indexOf(a.toLocaleLowerCase()) !== -1 && subset.indexOf(b.toLocaleLowerCase()) === -1) {
-							return -1;
-						} else if (subset.indexOf(a.toLocaleLowerCase()) === -1 && subset.indexOf(b.toLocaleLowerCase()) !== -1) {
-							return 1;
-						} else {
-							return a.toLocaleLowerCase() > b.toLocaleLowerCase();
-						}
-					});
-
-					// Remove current user from suggestions
-					if (app.user.username && usernames.indexOf(app.user.username) !== -1) {
-						usernames.splice(usernames.indexOf(app.user.username), 1);
-					}
-
-					callback(usernames);
-				});
-			},
-			index: 1,
-			replace: function (mention) {
-				mention = $('<div/>').html(mention).text();
-				return '@' + utils.slugify(mention, true) + ' ';
-			},
-			cache: true
-		};
-
-		data.strategies.push(strategy);
+	socket.emit('plugins.mentions.listGroups', function(err, groupNames) {
+		Mentions.groups = groupNames;
 	});
 
 	$(window).on('action:composer.loaded', function(e, data) {
-		var composer = $('#cmp-uuid-' + data.post_uuid + ' .write');
-		composer.attr('data-mentions', '1');
-	});
+		var composer = $('#cmp-uuid-' + data.post_uuid + ' .write'),
+			DOMusers = [];
 
-	function loadDomUsers() {
-		var DOMusers = [];
-		$('[component="post"][data-uid!="0"]').each(function(idx, el) {
+		// Retrieve DOM users
+		$('.posts [data-pid] .username-field').each(function(idx, el) {
 			var	username = el.getAttribute('data-username');
 			if (DOMusers.indexOf(username) === -1) {
 				DOMusers.push(username);
 			}
 		});
-		return DOMusers;
-	}
 
-	function loadGroupList() {
-		socket.emit('plugins.mentions.listGroups', function(err, groupNames) {
-			if (err) {
-				return app.alertError(err.message);
-			}
-			groupList = groupNames;
-		});
-	}
-
+		Mentions.addAutofill(composer, DOMusers);
+		$('.textcomplete-wrapper').css('height', '100%').find('textarea').css('height', '100%');
+	});
 });
+
+(function(window) {
+	window.Mentions = {
+		groups: null,
+		addAutofill: function(element, localUserList) {
+			var subset,
+				collapseSpaces = function(username) {
+					if (typeof username !== 'string') {
+						username = username.toString();
+					}
+
+					var html = '<a class="plugin-mentions-a" href="/community/user/' + utils.slugify(username) + '">' + username + '</a>';
+
+					return username.replace(/\s/g, '0');
+				};
+
+			element.textcomplete([{
+				match: /\B@([^\s\n]*)?$/,
+				search: function (term, callback) {
+					var usernames;
+					if (!term) {
+						usernames = localUserList.concat(Mentions.groups).filter(function(value, index, array) {
+							return array.indexOf(value) === index && value !== app.username;
+						}).sort(function(a, b) {
+							return a.toLocaleLowerCase() > b.toLocaleLowerCase();
+						})
+						return callback(usernames);
+					}
+
+					socket.emit('user.search', {query: term}, function(err, userdata) {
+						if (err) {
+							return callback([]);
+						}
+
+						usernames = userdata.users.map(function(user) {
+							return user.username;
+						});
+
+						subset = localUserList.concat(Mentions.groups).filter(function(username) {
+							return username.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) !== -1;
+						});
+
+						usernames = usernames.concat(subset).filter(function(value, index, array) {
+							return array.indexOf(value) === index;
+						});
+
+						subset = subset.map(function(name) {
+							return name.toLocaleLowerCase();
+						});
+
+						usernames.sort(function(a, b) {
+							if (subset.indexOf(a.toLocaleLowerCase()) !== -1 && subset.indexOf(b.toLocaleLowerCase()) === -1) {
+								return -1;
+							} else if (subset.indexOf(a.toLocaleLowerCase()) === -1 && subset.indexOf(b.toLocaleLowerCase()) !== -1) {
+								return 1;
+							} else {
+								return a.toLocaleLowerCase() > b.toLocaleLowerCase();
+							}
+						});
+
+						// Remove current user from suggestions
+						if (app.username && usernames.indexOf(app.username) !== -1) {
+							usernames.splice(usernames.indexOf(app.username), 1);
+						}
+
+						callback(usernames);
+					});
+				},
+				index: 1,
+				replace: function (mention) {
+					return '@' + collapseSpaces(mention);
+				},
+				cache: true
+			}]);
+
+			element.attr('data-mentions', '1');
+		}
+	};
+})(window);
